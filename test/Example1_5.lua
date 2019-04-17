857f17884e6aaf86a65d4f813279a4c5fc3e7ec2
Armando Arce <armando.arce@gmail.com>
2019-04-17 16:06:19 -0600

    Update Example1_5.lua

--- test/Example1_5.lua
+++ test/Example1_5.lua
@@ -1,20 +1,19 @@
-require "Legend"
+require "src/Legend"
 
 function setup()
 	size(640,320)
-	f = loadFont("data/Vera.ttf",11)
-	textFont(f)
 end
 
 function draw()
 	background(255)
 
 	local weekdays = {"Sun","Mon","Tue"}
-	local colors = {"red","blue","green"}
+	local colors = {0xFF0000,0x00FF00,0x0000FF}
 	pushMatrix()
 	translate(50,50)
-	Legend(10,10,weekdays,colors,VERTICAL,10)
+	Legend(10,10,10,10,weekdays,colors,VERTICAL,10)
 	popMatrix()
 	translate(150,50)
-	Legend(10,10,weekdays,colors,HORIZONTAL,15)
+	Legend(10,10,10,10,weekdays,colors,HORIZONTAL,15)
 end
+
